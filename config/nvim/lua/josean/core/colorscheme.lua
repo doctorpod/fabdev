3cfbd8269e9fbd6204dd1bbfba8b2eb3af2743da
Andy White <andy.white@digital.justice.gov.uk>
2023-05-10 15:46:33 +0100

    Which key tweaks

--- config/nvim/lua/josean/core/colorscheme.lua
+++ config/nvim/lua/josean/core/colorscheme.lua
@@ -5,3 +5,10 @@ if not status then
   print("Colorscheme not found!") -- print error if colorscheme not installed
   return
 end
+
+-- So that the bg of pop up menus (esp which key) are dark
+local status, _ = pcall(vim.cmd, "highlight Pmenu guifg=#ebdbb2 ctermfg=223 guibg=NONE ctermbg=237 gui=NONE cterm=NONE")
+if not status then
+  print("Unable to set Pmenu bg colour")
+  return
+end
